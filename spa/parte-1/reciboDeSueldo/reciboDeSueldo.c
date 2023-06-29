7b2b028c90e773e589bac7b5e3de296a5497948e
franco-ibanez-dev <francoibanez.dev@gmail.com>
2023-06-29 19:42:19 -0300

    [feature] Added solution for reciboDeSueldo problem (reciboDeSueldo.c)

--- spa/parte-1/reciboDeSueldo/reciboDeSueldo.c
+++ spa/parte-1/reciboDeSueldo/reciboDeSueldo.c
@@ -1,3 +1,18 @@
+/*
+Nombre del Programa: reciboDeSueldo
+Autor: Franco Ibañez
+Fecha de creación: 22/04/2023
+Fecha de modificación: 29/06/2023
+Versión: 1.1
+Nombre del archivo: reciboDeSueldo.c
+Descripción:
+  Este programa calcula el sueldo neto de un empleado. Se solicita información
+  como nombre, apellido, sueldo bruto, premio, comida, viáticos y días ausentes.
+  Luego se realizan cálculos para determinar el sueldo neto teniendo en cuenta
+  descuentos y beneficios adicionales. Finalmente, se muestra el resultado
+  en pantalla.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -38,10 +53,12 @@ int main()
 
   printf("\nEmpleado: %s %s\n", nombre, apellido);
   printf("\n\tSueldo Bruto: $%.2f\n", sueldoBruto);
-  printf("\n\tViático: $%.2f\n \tPremio: $%.2f\n \tComida: $%.2f\n", viaticos, premio, comida);
+  printf("\n\tViático: $%.2f\n \tPremio: $%.2f\n \tComida: $%.2f\n",
+         viaticos, premio, comida);
   printf("\nCantidad de días ausentes: %d\n", diasAusentes);
   printf("Por cada día se descuenta: $%.2f\n", montoPorAusentismo);
-  printf("\tMonto descontado por ausentimo: $%.2f\n", diasAusentes * montoPorAusentismo);
+  printf("\tMonto descontado por ausentimo: $%.2f\n",
+         diasAusentes * montoPorAusentismo);
   printf("\nSe descuenta por Jubilación: $%.2f\n", sueldoBruto * .11);
   printf("Se descuenta por Obra Social: $%.2f\n", sueldoBruto * .03);
   printf("Se descuenta por Ley 19032: $%.2f\n", sueldoBruto * .03);
